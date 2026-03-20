62c258089b98b79ee9796074ed777847f23ff8d2
MRBCreal <realmrb@gmail.com>
2026-03-20 13:47:51 -0300

    Restore original static frontend configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  trailingSlash: false,
+  trailingSlash: true,
   images: {
     unoptimized: true,
     domains: ['localhost', 'images.unsplash.com'],
